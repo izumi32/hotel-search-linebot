5f6bf2e6cd31ced26002c939978f2d8fe1be9e75
izumi32 <k.izumi721@gmail.com>
2021-11-24 23:16:52 +0900

    宿泊検索の返信機能を実装

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -9,16 +9,17 @@ class LineBotController < ApplicationController
     unless client.validate_signature(body, signature)
       return head :bad_request
     end
+    # bodyのevents以下を配列で受け取る
     events = client.parse_events_from(body)
     events.each do |event|
+      # メッセージイベント（ユーザーがメッセージを送信したイベント）か判定
       case event
       when Line::Bot::Event::Message
+        # eventのtypeキーがtextか判定（typeメソッド）
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
+          message = search_and_create_message(event.message['text'])
+          # 応答トークンを元にリプライを送信
           client.reply_message(event['replyToken'], message)
         end
       end
@@ -33,6 +34,40 @@ class LineBotController < ApplicationController
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+
+  def search_and_create_message(keyword)
+    # HTTPClientクラスをインスタンス化
+    http_client = HTTPClient.new
+    url = 'https://app.rakuten.co.jp/services/api/Travel/KeywordHotelSearch/20170426'
+    query = {
+      'keyword' => keyword,
+      'applicationId' => ENV['RAKUTEN_APPID'],
+      'hits' => 5,
+      'responseType' => 'small',
+      'formatVersion' => 2
+    }
+    # get(リクエストURL, パラメーターハッシュ)でAPIを叩く
+    response = http_client.get(url, query)
+    # 文字列からハッシュに変換
+    response = JSON.parse(response.body)
+
+    if response.key?('error')
+      text = "条件に該当する宿泊施設がみつかりません"
+    else
+      text = ""
+      response['hotels'].each do |hotel|
+        text <<
+          hotel[0]['hotelBasicInfo']['hotelName'] + "\n" +
+          hotel[0]['hotelBasicInfo']['hotelInformationUrl'] + "\n" +
+          "\n"
+      end
+    end
+
+    message = {
+      type: 'text',
+      text: text
     }
   end
 end
